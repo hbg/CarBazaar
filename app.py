19d9bdce4e035076fbaa16bbfd607c492d1bda63
Harris Beg <harris@harrisbeg.com>
2020-01-18 15:57:53 -0800

    Search Results

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_scss import Scss
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore, auth
 app = Flask(__name__)
 Scss(app)
+
+logged_user = None
 cred = credentials.Certificate('serviceaccount.json')
 firebase_admin.initialize_app(cred)
-logged_user = None
+VINS = [
+    {
+        "VIN"
+
+    }
+]
+
+def logged_in():
+    return logged_user is not None
+
 
 def get_mk_ml(s):
     return s.split('cars/')[1].split('/models/')[0], s.split('cars/')[1].split('/models/')[1]
@@ -20,13 +31,14 @@ def login():
 
         try:
             user = auth.get_user_by_email(request.form['email'])
-            assert request.form['password'] == user.password
+            #assert request.form['password'] == user.password
             logged_user = user
-            return render_template()
-        except:
+            return redirect('/garage')
+        except Exception as e:
+            print(e)
             "Incorrect password"
             return render_template("login.html", message="Invalid email or password.")
-    return render_template("login.html")
+    return render_template("login.html", logged_in=logged_in())
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -55,13 +67,21 @@ def search():
                 })
             car_entry = db.document(path).get()
             print(car_entry.to_dict())
-    return str(cars)
+    return render_template("search_results.html", cars=cars, logged_in=logged_in())
 
 
 @app.route('/')
 def home():
+    return render_template("index.html", logged_in=logged_in())
+
+
+@app.route('/garage')
+def garage():
+    global logged_user
     # Use the application default credentials
-    return render_template("index.html")
+    if logged_user is None:
+        return redirect('/login')
+    return render_template("home.html", logged_in=logged_in())
 
 
 if __name__ == '__main__':
