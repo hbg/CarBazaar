e1cc4f752271ba18197dac566a835f037a2ed074
Harris Beg <harris@harrisbeg.com>
2020-01-18 17:28:31 -0800

    Bugs

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, request, redirect
+from google.cloud.storage.blob import Blob
 from flask_scss import Scss
 import firebase_admin
-from firebase_admin import credentials
+import datetime
+from google.auth import compute_engine
+from google.auth.transport import requests
+
+from firebase_admin import credentials, storage
 from firebase_admin import firestore, auth
 app = Flask(__name__)
 Scss(app)
 
+
+
 logged_user = None
-cred = credentials.Certificate('serviceaccount.json')
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate('carbazaar-32cea-ec7ddc537cbe.json')
+fbapp = firebase_admin.initialize_app(cred, {
+    'storageBucket': 'carbazaar-32cea.appspot.com',
+})
+auth_request = requests.Request()
 VINS = [
     {
         "VIN"
@@ -40,6 +50,40 @@ def login():
             return render_template("login.html", message="Invalid email or password.")
     return render_template("login.html", logged_in=logged_in())
 
+
+def get_cars_from_user(email):
+    db = firestore.client()
+    garage = db.collection(u'users').document(email).collection('garage').stream()
+    cars = []
+    bucket = storage.bucket(app=fbapp)
+    signing_credentials = compute_engine.IDTokenCredentials(auth_request, "",
+                                                            service_account_email=cred.service_account_email)
+
+    for car in garage:
+        print(car.to_dict()['user_images'])
+        images = []
+        for image in car.to_dict()['user_images']:
+            try:
+                print(cred, image)
+                images.append(Blob.from_string(image).generate_signed_url(datetime.timedelta(seconds=300), credentials=signing_credentials))
+            except Exception as e:
+                print(e)
+                print("Improper URL")
+        print(images)
+        path = car.to_dict()['Model'].path
+        mk, ml = get_mk_ml(str(path))
+        mk = str(mk)
+        ml = str(ml)
+
+        cars.append({
+                'user': email,
+                "make": mk,
+                "model": ml,
+                "images": images,
+                "history": car.to_dict()['History']
+        })
+    return cars
+
 @app.route('/search', methods=['POST'])
 def search():
     db = firestore.client()
@@ -49,11 +93,13 @@ def search():
     users = db.collection(u'users').stream()
     user_ids = []
     cars = []
+    bucket = storage.bucket(app=fbapp)
     for user in users:
         user_ids.append(user.id)
     for user_id in user_ids:
         garage = db.collection(u'users').document(user_id).collection('garage').stream()
         for car in garage:
+            images = [bucket.blob(image).generate_signed_url(datetime.timedelta(seconds=300), method='GET') for image in car.to_dict()['user_images']]
             path = car.to_dict()['Model'].path
             mk, ml = get_mk_ml(str(path))
             mk = str(mk)
@@ -63,6 +109,7 @@ def search():
                     'user': user_id,
                     "make": mk,
                     "model": ml,
+                    "images": images,
                     "history": car.to_dict()['History']
                 })
             car_entry = db.document(path).get()
@@ -81,7 +128,8 @@ def garage():
     # Use the application default credentials
     if logged_user is None:
         return redirect('/login')
-    return render_template("home.html", logged_in=logged_in())
+
+    return render_template("home.html", logged_in=logged_in(), cars=get_cars_from_user(logged_user.email))
 
 
 if __name__ == '__main__':
